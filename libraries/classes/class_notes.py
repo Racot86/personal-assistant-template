2eba27b0dc1be84720d8066433fd55b68f5d8094
dmytromayevsky <mayevskydv@gmail.com>
2024-03-10 15:13:00 +0100

    added requrements.txt

--- libraries/classes/class_notes.py
+++ libraries/classes/class_notes.py
@@ -39,3 +39,6 @@ class Note:
 class NoteBook(UserList):
     def add_note(self, note: Note):
         self.data.append(note)
+
+    def delete_note(self, note: Note):
+        self.data.remove(note)
