38a84576654d58a7c37adf7ddd2f83ab169471f4
mariashubna <mariashubna@gmail.com>
2024-03-13 21:43:19 +0100

    /

--- src/functions/contacts/c_search_cmd.py
+++ src/functions/contacts/c_search_cmd.py
@@ -1,2 +1,44 @@
+# from src.tools.load_contacts import load_contacts
+# from src.tools.save_contacts import save_contacts
+# from src.classes.class_ContactBook import ContactBook
+# from src.classes.class_Contact import Contact
+
 def c_search_cmd(cmd):
-    print('contacts search command')
+    pass
+       
+    # if len(cmd) != 2:
+    #     print('My lord, I need to see the command and something about your friend... Write it down for me.')
+    #     return
+    # some = Contact('Dad')
+    # contacts = ContactBook(load_contacts())
+    # contact_data = cmd[1]
+    
+    # save_contacts(contacts)
+
+    # if '@' in contact_data and '.' in contact_data:
+    #     search_field = 'email'
+    # elif contact_data.isdigit() and len(contact_data) == 10:
+    #     search_field = 'phone'
+    # else:
+    #     search_field = 'name'
+
+    # found_contacts = []
+    # for contact in contacts:
+    #     if search_field == 'email' and contact_data.lower() in contact.email.lower():
+    #         found_contacts.append(contact)
+    #     elif search_field == 'phone' and contact_data in contact.phones:
+    #         found_contacts.append(contact)
+    #     elif search_field == 'name' and contact_data.lower() in contact.name.lower():
+    #         found_contacts.append(contact)
+
+    # if found_contacts:
+    #     for found_contact in found_contacts:
+    #         print(found_contact)
+    # else:
+    #     print('No contacts found matching your search.')
+
+    
+
+
+
+    
