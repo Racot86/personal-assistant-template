d13fc7535d241df9547a8e06bd51e22f99c3c968
mariashubna <mariashubna@gmail.com>
2024-03-13 22:00:08 +0100

    Update delete

--- src/functions/contacts/c_delete_cmd.py
+++ src/functions/contacts/c_delete_cmd.py
@@ -1,19 +1,10 @@
-# from src.tools.load_contacts import load_contacts
-# from src.tools.save_contacts import save_contacts
-# from src.classes.class_ContactBook import ContactBook
-# from src.classes.class_Contact import Contact
 from src.tools.StorageController import StorageController 
+# from src.classes.class_Contact import Contact
 
 
-# def c_delete_cmd(cmd):
-#     contacts = ContactBook(load_contacts())
-#     contact = Contact('Dmytro')
-#     contact.birthday = '04-06-1986'
-#     contacts.add_contact(contact)
-#     contacts.delete_contact(contact)
-#     print(contacts[0])
 
-#     save_contacts(contacts)
+
+
 
 
 def c_delete_cmd(cmd):
@@ -23,17 +14,18 @@ def c_delete_cmd(cmd):
     
     storage = StorageController()
     contacts = storage.load_contact_book()
-    # contacts = ContactBook(load_contacts())
+
+    # contact = Contact('Dmytro')
+    # contact.birthday = '04-06-1986'
+    # contacts.add_contact(contact)
 
     contact_name = cmd[1]
     contact = contacts.get_contact(contact_name)
     if contact:
         contacts.delete_contact(contact)
-        # save_contacts(contacts)
         storage.save_contact_book(contacts)
         print(f"Enemy '{contact_name}' is defeated by being destroyed.")        
     else:
         print(f"Enemy '{contact_name}' was early defeated. Write me new name.")
-
 
 
