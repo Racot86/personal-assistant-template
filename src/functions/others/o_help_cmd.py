3be50e5c12967cbaed904742f61bab8bf325d975
dmytromayevsky <mayevskydv@gmail.com>
2024-03-15 17:15:16 +0100

    changed help style

--- src/functions/others/o_help_cmd.py
+++ src/functions/others/o_help_cmd.py
@@ -1,5 +1,8 @@
-def o_help_cmd(cmd):
+from settings import Settings
+from src.tools.a_print import a_print
 
+
+def o_help_cmd(cmd):
     help_list = [
         {'cmd': 'notes filter by tag',
          'syntax': 'notes filter by tag <insert any number of tags with # and space between>',
@@ -11,42 +14,43 @@ def o_help_cmd(cmd):
          'description': '',  # Explanation what this command does
          'examples': ''  # examples of command usage
          },
-         {'cmd': "contacts show all",
+        {'cmd': "contacts show all",
          'syntax': "command does not require any additional parameters, it is used as is",
          'description': "this command shows the content of the contact book (fields name, phone number, remark)",
          'examples': "contacts show all"
          },
-         {'cmd': "contacts show <name>",
+        {'cmd': "contacts show",
          'syntax': "contacts filter by tag <name>, useы only one name, shows all the matches",
          'description': "This command searches and displays contacts filtered by birthdays in the next few days",
          'examples': "contacts show Dmitry"
          },
-         {'cmd': "contacts show birthdays <range>",
+        {'cmd': "contacts show birthdays <range>",
          'syntax': "filters the contact book by birthdays in the next <range> days",
          'description': "filters the contact book by birthdays in the next few days",
          'examples': "contacts show Dmitry"
          },
-         {'cmd': 'contacts delete',  
-         'syntax': 'contacts delete <name>', 
-         'description': 'This command deletes the specified contact',  
-         'examples': 'contacts delete name'  
+        {'cmd': 'contacts delete',
+         'syntax': 'contacts delete <name>',
+         'description': 'This command deletes the specified contact',
+         'examples': 'contacts delete name'
          },
-         {'cmd': 'contacts search',  
-         'syntax': 'contacts search <name> / contacts search <phone> / contacts search <email>', 
-         'description': 'This command searches for a contact by name, phone number or email',  
-         'examples': 'contacts search name / contacts search 0630004400 / contacts search test@email.com'  
+        {'cmd': 'contacts search',
+         'syntax': 'contacts search <name> / contacts search <phone> / contacts search <email>',
+         'description': 'This command searches for a contact by name, phone number or email',
+         'examples': 'contacts search name / contacts search 0630004400 / contacts search test@email.com'
          },
-          {'cmd': 'create',
+        {'cmd': 'notes create',
          'syntax': 'notes create <title> / <body>',
          'description': 'add note to note book',
          'examples': 'notes create <title: borscht> / <body:Ukrainian Borscht everyone knows'
          },
-           {'cmd': 'change',
+        {'cmd': 'notes change',
          'syntax': 'notes change <title> / <body>',
          'description': 'changes note',
-         'examples': 'notes change <title: borscht>  enter new title:<new_title>  enter new body:<new_body>'  # examples of command usage
+         'examples': 'notes change <title: borscht>  enter new title:<new_title>  enter new body:<new_body>'
+         # examples of command usage
          },
-           {'cmd': 'delete',
+        {'cmd': 'notes delete',
          'syntax': 'notes delete <title>',
          'description': 'delete note from note book',
          'examples': 'notes delete <title:borscht>'
@@ -56,9 +60,11 @@ def o_help_cmd(cmd):
     ]
 
     if len(cmd) == 1:
+        a_print('Doctor, please find below list of commands which I currently know', prefix='TARDIS: ',
+                main_color=Settings.msg_color)
         for itm in help_list:
-            print('Command name: ' + itm['cmd'])
-            print('Syntax: ' + itm['syntax'])
-            print('Description: ' + itm['description'])
-            print('Examples: ' + itm['examples'])
+            print(f'  {Settings.notes_color}Command name: {Settings.end_all}' + itm['cmd'])
+            print(f'  {Settings.notes_color}Syntax: {Settings.end_all}' + itm['syntax'])
+            print(f'  {Settings.notes_color}Description: {Settings.end_all}' + itm['description'])
+            print(f'  {Settings.notes_color}Examples: {Settings.end_all}' + itm['examples'])
             print()
