51b51aaa6e80c7baf499b24e3c1230ce6a6b1a88
RomanTuras <salden.com@gmail.com>
2024-03-15 19:50:23 +0200

    Bugfix for creating contacts

--- src/functions/contacts/c_create_cmd.py
+++ src/functions/contacts/c_create_cmd.py
@@ -18,30 +18,40 @@ def c_create_cmd(cmd):
     contacts = storage_controller.load_contact_book()
 
     if len(cmd)<2:
-        print(f"{Settings.msg_color}My Lord, you forget enter a name?{Settings.end_color}")
+        print(f"TARDIS: {Settings.msg_color}My Lord, you forget enter a name?{Settings.end_color}")
     else:
         if contacts.contact_exists(cmd[1]):
-            print(f"{Settings.error_color}Entry already exists, my Lord!{Settings.end_color}")
+            print(f"TARDIS: {Settings.error_color}Entry already exists, my Lord!{Settings.end_color}")
         else:
             contact = Contact(cmd[1])
             items = cmd[2:]
+            errors = []
             for item in items:
                 if 'phones' in item:
-                    add_contact_phones(contact, item)
+                    response = add_contact_phones(contact, item)
+                    if len(response)>0:
+                        errors.append(response)
                 elif 'email' in item:
-                    add_contact_email(contact, item)
+                    response = add_contact_email(contact, item)
+                    if len(response)>0:
+                        errors.append(response)
                 elif 'birthday' in item:
-                    add_contact_birthday(contact, item)
+                    response = add_contact_birthday(contact, item)
+                    if len(response)>0:
+                        errors.append(response)
 
-            answer = input(f"{Settings.msg_color}Add some address? (yes/no) {Settings.end_color}").lower()
-            if answer in ["y", "yes"]:
-                address = input(f"{Settings.msg_color}Type some address >> {Settings.end_color}")
-                contact.address = address
+            if len(errors)>0:
+                errors_str = ", ".join(errors)
+            #     print(f"TARDIS: {Settings.error_color}Wrong format in next fields: {errors_str}, see help, my Lord!{Settings.end_color}")
+            else:
+                answer = input(f"TARDIS: {Settings.msg_color}Add some address? (yes/no) {Settings.end_color}").lower()
+                if answer in ["y", "yes"]:
+                    address = input(f"TARDIS: {Settings.msg_color}Type some address: {Settings.end_color}")
+                    contact.address = address
 
-            print(f"{Settings.success_color}Contact added successfuly!{Settings.end_color}")
-            contacts.add_contact(contact)
-            storage_controller.save_contact_book(contacts)
-            print(contacts) #TODO: remove after testing?
+                print(f"TARDIS: {Settings.success_color}Contact added successfuly!{Settings.end_color}")
+                contacts.add_contact(contact)
+                storage_controller.save_contact_book(contacts)
 
 
 
@@ -49,15 +59,27 @@ def add_contact_phones(contact, string_phones:str):
     '''Add phones to contact'''
     phones = string_phones[7:]
     contact.phones = phones.split(',')
+    error = ''
+    if len(contact.phones)==0:
+        error = 'phones'
+    return error
 
 
 def add_contact_email(contact, string_email:str):
     '''Add email to contact'''
     email = string_email[6:]
     contact.email = email
+    error = ''
+    if contact.email=='':
+        error = 'email'
+    return error
 
 
 def add_contact_birthday(contact, string_birthday:str):
     '''Add birthday to contact'''
     birthday = string_birthday[9:]
     contact.birthday = birthday
+    error = ''
+    if contact.birthday=='':
+        error = 'birthday'
+    return error
