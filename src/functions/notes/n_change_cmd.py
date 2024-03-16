00e4cb83d133f3c32cde17fa0aca4a2bae9601ce
misfits3z <3zmisfitscrew@gmail.com>
2024-03-16 10:47:30 +0800

    added TARDIS and a_print

--- src/functions/notes/n_change_cmd.py
+++ src/functions/notes/n_change_cmd.py
@@ -3,6 +3,7 @@ from src.tools.StorageController import StorageController
 from prompt_toolkit.styles import Style
 from settings import Settings
 from prompt_toolkit import prompt
+from src.tools.a_print import a_print
 
 style = Style.from_dict({'': Settings.PROMPT_INPUT_COLOR, 'prompt': Settings.PROMPT_TEXT_COLOR })
 
@@ -13,27 +14,28 @@ def n_change_cmd(cmd):
     notes = storage.load_note_book()
     title = " ".join(cmd[1:])
     if not notes.note_exists(title): 
-        print(f"{Settings.error_color}Note with provided title not found{Settings.end_color}")
+        a_print("Note with provided title not found", prefix="TARDIS: ", main_color=Settings.error_color)
     else:
         print("TARDIS: ")
-        new_title = prompt("  Enter new title> ", default=title, style=style)
+        new_title = prompt("   Enter new title> ", default=title, style=style)
 
         note = [note for note in notes if note.title == title][0]
         note.title = new_title
         print(f"{Settings.shadow_color}Time: {Settings.end_all}{note.time}")
         print(f"{Settings.shadow_color}Title: {Settings.end_all}{new_title}")
         print(f"{Settings.shadow_color}Body:{Settings.end_all}\n{Settings.notes_color}\n{note.body}\n{Settings.end_color}\n")
-        print(f"{Settings.success_color}Title updated successfully!{Settings.end_color}")
+        a_print("Title updated successfully!", prefix="TARDIS: ", main_color=Settings.success_color)
         storage.save_note_book(notes)
 
         current_body = note.body
 
-        new_text = prompt("Enter new body>", multiline=True, default=current_body,style=style)
+        print("TARDIS: ")
+        new_text = prompt("   Enter new body>", multiline=True, default=current_body,style=style)
         note.body = new_text
         print(f"{Settings.shadow_color}Time: {Settings.end_all}{note.time}")
         print(f"{Settings.shadow_color}Title: {Settings.end_all}{note.title}")
         print(f"{Settings.shadow_color}Body:{Settings.end_all}\n{Settings.notes_color}\n{new_text}\n{Settings.end_color}\n")
-        print(f"{Settings.success_color}Note updated successfully!!{Settings.end_color}")
+        a_print("Note updated successfully!!", prefix="TARDIS: ", main_color=Settings.success_color)
         storage.save_note_book(notes)
         
 
